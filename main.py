c2aa91a3e4cceceba92339955dbccb78a5414d29
Poxxon <>
2024-10-05 00:28:44 -0400

    Added spacing for better readability

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ files = os.listdir(source_dir)
 
 # Loop through each file in the source directory
 for file_name in files:
+
     # Check if file ends with .pdf and contains 'intern', 'coop' or 'winter'
     if file_name.endswith('.pdf') and any(keyword in file_name.lower() for keyword in 
     ['intern', 'coop', 'co-op', 'winter']):
+        
         # Build the full file path
         source_file = os.path.join(source_dir, file_name)
         dest_file = os.path.join(dest_dir, file_name)
