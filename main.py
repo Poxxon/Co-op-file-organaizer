1c5cbd8a3cf3dbed5a4d60e6236694d14c8b754b
Poxxon <>
2024-10-04 23:51:53 -0400

    If folder does not exist create one

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import shutil #shell utilities
 # Specify the source directory and dest directory
 source_dir = '/Users/pouyarad/Downloads'
 dest_dir = '/Users/pouyarad/Downloads/Co-op Resume/CV'
+
+# If file does not exist in the directory create the respective folder
+if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
