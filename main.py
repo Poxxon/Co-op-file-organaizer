8965a0daa2424b95400dd1b71b621dea81c06b55
Poxxon <>
2024-10-04 23:46:14 -0400

    Added libraries and source and destination directories

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+import shutil #shell utilities
+
+# Specify the source directory and dest directory
+source_dir = '/Users/pouyarad/Downloads'
+dest_dir = '/Users/pouyarad/Downloads/Co-op Resume/CV'
